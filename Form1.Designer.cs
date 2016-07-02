b4483921c1479d8ba4cafed23f1d3f821ed54932
Jose Gerardo Martinez Moran <geramtz100@gmail.com>
2016-07-02 01:09:08 -0500

    Proyecto Original
    Programa funcional

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace EasyPrinting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lcosto = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lcrd = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EasyPrinting.Properties.Resources.Logotipo_PNG__649799_;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(615, 531);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Creditos: $";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(704, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Seleccione una unidad:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(940, 360);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 32);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Seleccione";
+            this.comboBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(616, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(318, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Seleccione el archivo a imprimir:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(940, 398);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(217, 32);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "Seleccione";
+            this.comboBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comboBox2_KeyUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(741, 442);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Costo de impresion: $";
+            // 
+            // lcosto
+            // 
+            this.lcosto.AutoSize = true;
+            this.lcosto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lcosto.Location = new System.Drawing.Point(940, 442);
+            this.lcosto.Name = "lcosto";
+            this.lcosto.Size = new System.Drawing.Size(20, 24);
+            this.lcosto.TabIndex = 9;
+            this.lcosto.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(940, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(284, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "IMPRIMIR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.UseWaitCursor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button3_KeyUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 11;
+            // 
+            // lcrd
+            // 
+            this.lcrd.AutoSize = true;
+            this.lcrd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lcrd.Location = new System.Drawing.Point(777, 532);
+            this.lcrd.Name = "lcrd";
+            this.lcrd.Size = new System.Drawing.Size(30, 31);
+            this.lcrd.TabIndex = 12;
+            this.lcrd.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(503, 182);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "comodidad y rapidez a tu servicio";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(633, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(606, 33);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Bienvenido a nuestro centro de impresión";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(563, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 22);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Instrucciones:";
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(564, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(675, 188);
+            this.label9.TabIndex = 16;
+            this.label9.Text = resources.GetString("label9.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 659);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lcrd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lcosto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Easy Printing";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lcosto;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lcrd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Printing;
+using System.IO.Ports;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+using System.Threading;
+
+
+namespace EasyPrinting
+{
+    public partial class Form1 : Form
+    {
+        List<string> arch = new List<string>();
+        //Thread hilo;
+        Thread ohilo;
+        SerialPort ard = new SerialPort();
+        DriveInfo[] allDrives;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string unidad;
+        private int pag;
+        private int cre;
+
+        public string u
+        {
+            get
+            {
+                return unidad;
+            }
+            set
+            {
+                this.unidad = value;
+            }
+        }
+
+        public int p
+        {
+            get
+            {
+                return pag;
+            }
+            set
+            {
+                this.pag = value;
+            }
+        }
+
+        public int c
+        {
+            get
+            {
+                return cre;
+            }
+            set
+            {
+                this.cre = value;
+            }
+        }
+
+        //public void abrirpuerto()
+        //{
+        //    ard.BaudRate = 9600;
+        //    ard.PortName = "COM5";
+        //    ard.Open();
+        //}
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Focus();
+            try
+            {
+                //abrirpuerto();
+                CheckForIllegalCrossThreadCalls = false;
+                p = 0;
+                c = 0;
+                funcion1();
+                //ard.WriteLine("i");
+                //hilo = new Thread(new ThreadStart(hilo1));
+                ohilo = new Thread(new ThreadStart(hilo2));
+                //hilo.Start();
+                ohilo.Start();
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        //public void hilo1()
+        //{
+        //    while (true)
+        //    {
+        //        c = coins();
+        //        lcrd.Text = c.ToString();
+        //        Thread.Sleep(1500);
+        //    }
+        //}
+
+        public void hilo2()
+        {
+            while (true)
+            {
+                if(c >= p && p != 0)
+                {
+                    button3.BackColor = Color.Green;
+                    button3.Focus();
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        public void funcion1()
+        {
+            allDrives = DriveInfo.GetDrives();
+            foreach (var a in allDrives)
+            {
+                if (a.IsReady != false && a.DriveType == DriveType.Removable)
+                {
+                    comboBox1.Items.Add(a.Name);
+                }
+            }
+        }
+
+        public void funcion2()
+        {
+            arch.Clear();
+            comboBox2.Items.Clear();
+            u = comboBox1.SelectedItem.ToString();
+            DriveInfo uni = new DriveInfo(@u);
+            DirectoryInfo root = uni.RootDirectory;
+            string[] files = Directory.GetFiles(u, "*.doc");
+            foreach (string s in files)
+            {
+                FileInfo f = null;
+                try
+                {
+                    f = new FileInfo(s);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    continue;
+                }
+                string ft = string.Format("{0}", f.Name);
+                comboBox2.Items.Add(ft);
+                arch.Add(s.ToString());
+            }
+        }
+
+        //public int coins()
+        //{
+        //    int coin = 0;
+        //    try
+        //    {
+        //        coin = int.Parse(ard.ReadLine());
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //    }
+        //    return coin;
+        //}
+
+        public void funcion3()
+        {
+
+            var app = new Microsoft.Office.Interop.Word.Application();
+            var document = app.Documents.Open(@arch[comboBox2.SelectedIndex]);
+            var nop = document.ComputeStatistics(WdStatistic.wdStatisticPages, false);
+            lcosto.Text = nop.ToString();
+            document.Close();
+            app.Quit();
+            p = nop;
+        }
+
+        public void borrar()
+        {
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox1.Text = "Seleccione";
+            comboBox2.Text = "Seleccione";
+            button3.BackColor = Color.Red;
+            comboBox2.Enabled = false;
+            comboBox1.Focus();
+            p = 0;
+            lcosto.Text = "0";
+            //ard.WriteLine("i");
+            //c = coins();
+            lcrd.Text = c.ToString();
+            u = "";
+            funcion1();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void comboBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if(comboBox1.SelectedIndex != -1)
+                    {
+                        funcion2();
+                        comboBox2.Enabled = true;
+                        comboBox2.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar una unidad");
+                    }
+                    break;
+
+                case Keys.Insert:
+                    comboBox1.Items.Clear();
+                    comboBox1.Text = "";
+                    funcion1();
+                    break;
+            }
+        }
+
+        private void comboBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Insert:
+                    comboBox2.Items.Clear();
+                    comboBox2.Text = "";
+                    funcion2();
+                    button3.Focus();
+                    break;
+
+                case Keys.Right:
+                    if (comboBox2.SelectedIndex != -1)
+                    {
+                        funcion3();
+                        button3.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar un documento");
+                    }
+                    break;
+
+                case Keys.End:
+                    if (comboBox2.SelectedIndex != -1)
+                    {
+                        funcion3();
+                        button3.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar un documento");
+                    }
+                    break;
+            }
+        }
+
+        public void funcion4()
+        {
+            DialogResult q = MessageBox.Show("¿Deseas imprimir el documento?", "", MessageBoxButtons.YesNo);
+            if (q == DialogResult.Yes)
+            {
+                var app = new Microsoft.Office.Interop.Word.Application();
+                var document = app.Documents.Open(@arch[comboBox2.SelectedIndex]);
+                document.PrintOut();
+                document.Close();
+                app.Quit();
+                MessageBox.Show("Su documento ha sido impreso, Gracias por usar nuestro servicio :)");
+                int res = c - p;
+                c = res;
+                borrar();
+            }
+        }
+
+        private void button3_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.End:
+                    if (c >= p)
+                    {
+                        funcion4();
+                    }
+                    else
+                    {
+                        MessageBox.Show("favor de introducir los creditos necesarios para imprimir");
+                    }
+                    break;
+
+                case Keys.Home:
+                    DialogResult hi = MessageBox.Show("¿Deseas cancelar la impresion y empezar de nuevo?, Los creditos se conservan", "", MessageBoxButtons.YesNo);
+                    if(hi == DialogResult.Yes)
+                    {
+                        borrar();
+                    }
+                    break;
+                case Keys.Space:
+                    c += 1;
+                    lcrd.Text = c.ToString();
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.E:
+                    if (c >= p)
+                    {
+                        funcion4();
+                    }
+                    else
+                    {
+                        MessageBox.Show("falta datos a introducir");
+                    }
+                    break;
+
+                case Keys.Space:
+                    c += 1;
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
